de927cd0028d6ce0a118ddf0df588cea950ded29
jhy950210 <jhyy1995210@gamil.com>
2024-06-11 10:05:16 +0900

    init

---
+++ src/main/kotlin/com/plate/hy/config/SecurityConfig.kt
@@ -0,0 +1,29 @@
+package com.plate.hy.config
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    private val allowedUrls = arrayOf("/",  "/swagger-ui/**", "/v3/**", "/sign-up", "/sign-in", "/h2-console/**")
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+
+    @Bean
+    fun filterChain(http: HttpSecurity) = http
+        .csrf { it.disable() }
+        .headers { it.frameOptions { frameOptions -> frameOptions.sameOrigin() } }
+        .authorizeHttpRequests {
+            it.requestMatchers(*allowedUrls).permitAll()
+                .anyRequest().authenticated()
+        }
+        .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+        .build()!!
+}
